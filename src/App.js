069b1fe5919a7cc577cde0d5d410e490fb456a17
Erol Yurtkuran <erol@yurtkuran.net>
2020-04-24 07:11:19 -0700

    components and UI complete

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Fragment, useEffect } from 'react';
+
+// bring in components
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+
+// bring in materialize
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import './App.css';
+
+const App = () => {
+    useEffect(() => {
+        // init materialize JS
+        M.AutoInit();
+    });
+    return (
+        <Fragment>
+            <SearchBar />
+            <div className='container'>
+                <AddBtn />
+                <AddLogModal />
+                <EditLogModal />
+                <AddTechModal />
+                <TechListModal />
+                <Logs />
+            </div>
+        </Fragment>
+    );
+};
+
+export default App;
